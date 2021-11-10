b6738ff54d50add61b685f6ef96f45b55723c043
n880510n <33122663+n880510n@users.noreply.github.com>
2021-11-10 10:46:50 +0800

    Initial commit

---
+++ app/src/main/java/ian/com/minigame/MainActivity2.java
@@ -0,0 +1,26 @@
+package ian.com.minigame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private TextView res;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        res = (TextView)findViewById(R.id.res);
+        Intent intent = this.getIntent();
+        res.setText(intent.getStringExtra("wrong"));
+    }
+
+    public void back(View view){
+        MainActivity2.this.finish();
+    }
+}
